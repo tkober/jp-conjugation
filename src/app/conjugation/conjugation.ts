74f7afaa2c65859a94a1fecf63fcce90e0074723
Thorsten Kober <thorsten.kober@softgarden.de>
2023-11-16 01:41:50 +0100

    Conjugation strategy pattern

---
+++ src/app/conjugation/conjugation.ts
@@ -0,0 +1,25 @@
+export enum WordType {
+    IchidanVerb = 'ichidan_verb',
+    GodanVerb = 'godan_verb',
+    SuruVerb = 'suru_verb',
+    KuruVerb = 'kuru_verb',
+    IAdjective = 'i_adjective',
+    NaAdjective = 'na_adjective'
+}
+
+export class Word {
+    public kanji: string;
+    public furigana: string;
+    public wordType: WordType;
+
+    constructor(kanji: string, furigana: string, wordType: WordType) {
+        this.kanji = kanji
+        this.furigana = furigana
+        this.wordType = wordType;
+    }
+}
+export interface Conjugation {
+
+    getConjugation(word: Word): Word
+
+}
---
+++ src/app/conjugation/forms/imperative-affirmative.ts
@@ -0,0 +1,26 @@
+import {Conjugation, Word, WordType} from "../conjugation";
+
+class ImperativeAffirmative implements Conjugation {
+    getConjugation(word: Word): Word {
+        switch (word.wordType) {
+            case WordType.GodanVerb:
+                return new Word('TODO', 'TODO', word.wordType);
+
+            case WordType.IchidanVerb:
+                return new Word('TODO', 'TODO', word.wordType);
+
+            case WordType.SuruVerb:
+                return new Word('TODO', 'TODO', word.wordType);
+
+            case WordType.KuruVerb:
+                return new Word('TODO', 'TODO', word.wordType);
+
+            case WordType.IAdjective:
+                return new Word('TODO', 'TODO', word.wordType);
+
+            case WordType.NaAdjective:
+                return new Word('TODO', 'TODO', word.wordType);
+        }
+    }
+
+}
