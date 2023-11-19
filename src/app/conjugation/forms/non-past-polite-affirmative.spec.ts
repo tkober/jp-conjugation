bc98a31e458a4d06b7284f7e2e5d9baa9acb7c89
Thorsten Kober <thorsten.kober@softgarden.de>
2023-11-19 01:02:17 +0100

    Implemented tests for NonPastPoliteAffirmative

---
+++ src/app/conjugation/forms/non-past-polite-affirmative.spec.ts
@@ -0,0 +1,53 @@
+import {NonPastPoliteAffirmative} from "./non-past-polite-affirmative";
+import {Word, WordType} from "../conjugation";
+
+describe('NonPastPoliteAffirmative', () => {
+
+    it('Ichidan Verb', () => {
+        const word = new Word('食べる', 'たべる', WordType.IchidanVerb)
+        const result = new NonPastPoliteAffirmative().getConjugation(word)
+
+        expect(result !== undefined).toBeTruthy()
+        expect(result).toEqual(new Word('食べます', 'たべます', WordType.IchidanVerb));
+    });
+
+    it('Godan Verb', () => {
+        const word = new Word('行く', 'いく', WordType.GodanVerb)
+        const result = new NonPastPoliteAffirmative().getConjugation(word)
+
+        expect(result !== undefined).toBeTruthy()
+        expect(result).toEqual(new Word('行きます', 'いきます', WordType.GodanVerb))
+    });
+
+    it('Suru Verb', () => {
+        const word = new Word('勉強', 'べんきょう', WordType.SuruVerb)
+        const result = new NonPastPoliteAffirmative().getConjugation(word)
+
+        expect(result !== undefined).toBeTruthy()
+        expect(result).toEqual(new Word('勉強します', 'べんきょうします', WordType.SuruVerb))
+    });
+
+    it('Kuru Verb', () => {
+        const word = new Word('来る', 'くる', WordType.KuruVerb)
+        const result = new NonPastPoliteAffirmative().getConjugation(word)
+
+        expect(result !== undefined).toBeTruthy()
+        expect(result).toEqual(new Word('来ます', 'きます', WordType.KuruVerb))
+    });
+
+    it('I-Adjective', () => {
+        const word = new Word('美味しい', 'おいしい', WordType.IAdjective)
+        const result = new NonPastPoliteAffirmative().getConjugation(word)
+
+        expect(result !== undefined).toBeTruthy()
+        expect(result).toEqual(new Word('美味しいです', 'おいしいです', WordType.IAdjective))
+    });
+
+    it('I-Adjective', () => {
+        const word = new Word('好き', 'すき', WordType.NaAdjective)
+        const result = new NonPastPoliteAffirmative().getConjugation(word)
+
+        expect(result !== undefined).toBeTruthy()
+        expect(result).toEqual(new Word('好きです', 'すきです', WordType.NaAdjective))
+    });
+})
