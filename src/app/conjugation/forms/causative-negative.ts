664740fbda62f4931dac6ddb983d02898d72762b
Thorsten Kober <thorsten.kober@softgarden.de>
2023-11-20 03:35:50 +0100

    Implemented causative, negative

--- src/app/conjugation/forms/causative-negative.ts
+++ src/app/conjugation/forms/causative-negative.ts
@@ -1,26 +1,17 @@
 import {Conjugation, Word, WordType} from "../conjugation";
+import {NonPastShortNegative} from "./non-past-short-negative";
+import {CausativeAffirmative} from "./causative-affirmative";
 
 export class CausativeNegative implements Conjugation {
     getConjugation(word: Word): (Word | undefined) {
-        switch (word.wordType) {
 
-            // Verbs
-            case WordType.GodanVerb:
-                return new Word('TODO', 'TODO', word.wordType);
-
-            case WordType.IchidanVerb:
-                return new Word('TODO', 'TODO', word.wordType);
-
-            case WordType.SuruVerb:
-                return new Word('TODO', 'TODO', word.wordType);
-
-            case WordType.KuruVerb:
-                return new Word('TODO', 'TODO', word.wordType);
-
-
-            default:
-                return undefined;
+        const originalType = word.wordType;
+        const causative = new CausativeAffirmative().getConjugation(word);
+        if (causative === undefined) {
+            return undefined;
         }
+
+        return new NonPastShortNegative().getConjugation(causative.changeType(WordType.IchidanVerb))?.changeType(originalType)
     }
 
 }
