29dbf3d4bdf96aff002fc61dd9b3b9a22781f061
Thorsten Kober <thorsten.kober@softgarden.de>
2023-11-20 04:16:06 +0100

    Implemented past, polite, affirmative

--- src/app/conjugation/forms/past-polite-affirmative.ts
+++ src/app/conjugation/forms/past-polite-affirmative.ts
@@ -1,4 +1,5 @@
 import {Conjugation, Word, WordType} from "../conjugation";
+import {HIRAGANA} from "../hiragana";
 
 export class PastPoliteAffirmative implements Conjugation {
     getConjugation(word: Word): (Word | undefined) {
@@ -6,16 +7,17 @@ export class PastPoliteAffirmative implements Conjugation {
 
             // Verbs
             case WordType.GodanVerb:
-                return new Word('TODO', 'TODO', word.wordType);
+                const lastKana = HIRAGANA[word.getLastKana()]
+                return word.replaceLastKana(lastKana.getGroup().i + 'ました')
 
             case WordType.IchidanVerb:
-                return new Word('TODO', 'TODO', word.wordType);
+                return word.replaceLastKana('ました');
 
             case WordType.SuruVerb:
-                return new Word('TODO', 'TODO', word.wordType);
+                return word.addSuffix('しました');
 
             case WordType.KuruVerb:
-                return new Word('TODO', 'TODO', word.wordType);
+                return new Word('来ました', 'きました', word.wordType);
 
 
             // Adjectives
