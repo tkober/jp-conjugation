98846cf98db24a33fad42e64c1b3b2fb4de894ac
Thorsten Kober <thorsten.kober@softgarden.de>
2023-11-20 04:04:30 +0100

    Implemented te-form, affirmative

--- src/app/conjugation/forms/te-form-affirmative.ts
+++ src/app/conjugation/forms/te-form-affirmative.ts
@@ -1,4 +1,5 @@
 import {Conjugation, Word, WordType} from "../conjugation";
+import {HIRAGANA} from "../hiragana";
 
 export class TeFormAffirmative implements Conjugation {
     getConjugation(word: Word): (Word | undefined) {
@@ -6,16 +7,48 @@ export class TeFormAffirmative implements Conjugation {
 
             // Verbs
             case WordType.GodanVerb:
-                return new Word('TODO', 'TODO', word.wordType);
+                const lastKana = HIRAGANA[word.getLastKana()]
+
+                //　行く
+                if (word.kanji === '行く') {
+                    return new Word('行って', 'いって', word.wordType)
+                }
+
+                //　う、つ、る　ー＞　って
+                if (['う', 'つ', 'る'].indexOf(lastKana.kana) !== -1) {
+                    return word.replaceLastKana('って')
+                }
+
+                //　む、ぶ、ぬ　ー＞　んで
+                if (['む', 'ぶ', 'ぬ'].indexOf(lastKana.kana) !== -1) {
+                    return word.replaceLastKana('んで')
+                }
+
+                //　く　ー＞　いて
+                if (lastKana.kana === 'く') {
+                    return word.replaceLastKana('いて')
+                }
+
+                //　ぐ　ー＞　いで
+                if (lastKana.kana === 'ぐ') {
+                    return word.replaceLastKana('いで')
+                }
+
+                //　す　ー＞　して
+                if (lastKana.kana === 'す') {
+                    return word.replaceLastKana('して')
+                }
+
+                return undefined;
 
             case WordType.IchidanVerb:
-                return new Word('TODO', 'TODO', word.wordType);
+                return word.replaceLastKana('て');
 
             case WordType.SuruVerb:
-                return new Word('TODO', 'TODO', word.wordType);
+                return word.addSuffix('して');
 
             case WordType.KuruVerb:
-                return new Word('TODO', 'TODO', word.wordType);
+                return new Word('来て', 'きて', word.wordType);
 
 
             default:
