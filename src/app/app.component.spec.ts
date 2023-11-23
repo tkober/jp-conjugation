0fae27111106a2a20eae01fce09cb2d2d00653df
Thorsten Kober <thorsten.kober@softgarden.de>
2023-11-23 04:20:16 +0100

    Fixed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jp-conjugation'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('jp-conjugation');
-  });
-
 });
