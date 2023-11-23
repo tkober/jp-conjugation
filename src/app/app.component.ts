0fca9999ec07e5607bd04064cf33877b43456879
Thorsten Kober <thorsten.kober@softgarden.de>
2023-11-23 04:01:15 +0100

    Implemented basic answer form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import * as wanakana from 'wanakana';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -8,14 +9,38 @@ import * as wanakana from 'wanakana';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-    title = 'jp-conjugation';
+    public answerFormControl = new FormControl('', [
+        Validators.required,
+        AppComponent.isHiragana
+    ])
+    public formGroup = new FormGroup({
+        'answer': this.answerFormControl
+    });
 
     @ViewChild('answerInput') answerInput: ElementRef;
 
     ngOnInit() {
+
     }
 
     ngAfterViewInit(): void {
         wanakana.bind(this.answerInput.nativeElement)
+    }
+
+    checkAnswer() {
+        console.log('checkAnswer()')
+        console.log(wanakana.toHiragana(this.answerFormControl.value))
+    }
+
+    private static isHiragana(formControl: FormControl) {
+        const value = wanakana.toHiragana(formControl.value)
+        if (!wanakana.isHiragana(value)) {
+            return {
+                invalidAnswer: {
+                    message: 'Please write your answer in Kana.'
+                }
+            }
+        }
+        return null;
     }
 }
