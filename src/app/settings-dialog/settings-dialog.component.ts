3a1e5e0491a12729141d892b68910bca3f24bc0f
Thorsten Kober <thorsten.kober@softgarden.de>
2024-09-23 10:49:15 +0900

    Settings is now using the new structure.

--- src/app/settings-dialog/settings-dialog.component.ts
+++ src/app/settings-dialog/settings-dialog.component.ts
@@ -2,56 +2,18 @@ import {Component, OnInit} from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
 import {
     AdjectiveForms,
-    Adjectives__NonPastPoliteAffirmative,
-    Adjectives__NonPastPoliteNegative,
-    Adjectives__NonPastShortAffirmative,
-    Adjectives__NonPastShortNegative,
-    Adjectives__PastPoliteAffirmative,
-    Adjectives__PastPoliteNegative,
-    Adjectives__PastShortAffirmative,
-    Adjectives__PastShortNegative,
+    Adjectives__NonePast_Forms,
+    Adjectives__Past_Forms,
     Conjugation,
-    Verbs__CausativeAffirmative,
-    Verbs__CausativeNegative,
-    Verbs__CausativePassiveAffirmative,
-    Verbs__CausativePassiveNegative,
-    Verbs__ImperativeAffirmative,
-    Verbs__ImperativeNegative,
-    Verbs__NonPastPoliteAffirmative,
-    Verbs__NonPastPoliteNegative,
-    Verbs__NonPastShortAffirmative,
-    Verbs__NonPastShortNegative,
-    Verbs__PassiveAffirmative,
-    Verbs__PassiveNegative,
-    Verbs__PastPoliteAffirmative,
-    Verbs__PastPoliteNegative,
-    Verbs__PastShortAffirmative,
-    Verbs__PastShortNegative,
-    Verbs__PotentialAffirmative,
-    Verbs__PotentialNegative,
-    Verbs__TeFormAffirmative,
-    Verbs__TeFormNegative
+    Verbs__Causative_Forms,
+    Verbs__CausativePassive_Forms,
+    Verbs__Imperative_Forms,
+    Verbs__NonPast_Forms,
+    Verbs__Passive_Forms,
+    Verbs__Past_Forms,
+    Verbs__Potential_Forms,
+    Verbs__TeForm_Forms
 } from "../conjugation/conjugation";
-import {NonPastShortAffirmative} from "../conjugation/forms/non-past-short-affirmative";
-import {NonPastShortNegative} from "../conjugation/forms/non-past-short-negative";
-import {NonPastPoliteAffirmative} from "../conjugation/forms/non-past-polite-affirmative";
-import {NonPastPoliteNegative} from "../conjugation/forms/non-past-polite-negative";
-import {PastShortAffirmative} from "../conjugation/forms/past-short-affirmative";
-import {PastShortNegative} from "../conjugation/forms/past-short-negative";
-import {PastPoliteAffirmative} from "../conjugation/forms/past-polite-affirmative";
-import {PastPoliteNegative} from "../conjugation/forms/past-polite-negative";
-import {TeFormAffirmative} from "../conjugation/forms/te-form-affirmative";
-import {TeFormNegative} from "../conjugation/forms/te-form-negative";
-import {PotentialAffirmative} from "../conjugation/forms/potential-affirmative";
-import {PotentialNegative} from "../conjugation/forms/potential-negative";
-import {PassiveAffirmative} from "../conjugation/forms/passive-affirmative";
-import {PassiveNegative} from "../conjugation/forms/passive-negative";
-import {CausativeAffirmative} from "../conjugation/forms/causative-affirmative";
-import {CausativeNegative} from "../conjugation/forms/causative-negative";
-import {CausativePassiveAffirmative} from "../conjugation/forms/causative-passive-affirmative";
-import {CausativePassiveNegative} from "../conjugation/forms/causative-passive-negative";
-import {ImperativeAffirmative} from "../conjugation/forms/imperative-affirmative";
-import {ImperativeNegative} from "../conjugation/forms/imperative-negative";
 import {LocalStorageKey_ExcludedConjugations, LocalStorageKey_ExcludedJpltLevels, PracticeService} from "../services/practice.service";
 
 
@@ -74,6 +36,15 @@ class ConjugationGroup {
         this.title = title;
         this.items = items;
     }
+
+    static groupFromForms(name: string, forms: any): ConjugationGroup {
+        let items = [];
+        for (const form in forms) {
+            items.push(new ConjugationGroupItem(form, forms[form]));
+        }
+
+        return new ConjugationGroup(name, items);
+    }
 }
 
 class VocabularyItem {
@@ -95,65 +66,19 @@ class VocabularyItem {
 export class SettingsDialogComponent implements OnInit {
 
     public adjectiveConjugationGroups = [
-        new ConjugationGroup('Non-past', [
-            new ConjugationGroupItem(Adjectives__NonPastShortAffirmative, new NonPastShortAffirmative()),
-            new ConjugationGroupItem(Adjectives__NonPastShortNegative, new NonPastShortNegative(),),
-            new ConjugationGroupItem(Adjectives__NonPastPoliteAffirmative, new NonPastPoliteAffirmative(),),
-            new ConjugationGroupItem(Adjectives__NonPastPoliteNegative, new NonPastPoliteNegative()),
-        ]),
-
-        new ConjugationGroup('Past', [
-            new ConjugationGroupItem(Adjectives__PastShortAffirmative, new PastShortAffirmative()),
-            new ConjugationGroupItem(Adjectives__PastShortNegative, new PastShortNegative(),),
-            new ConjugationGroupItem(Adjectives__PastPoliteAffirmative, new PastPoliteAffirmative(),),
-            new ConjugationGroupItem(Adjectives__PastPoliteNegative, new PastPoliteNegative()),
-        ]),
+        ConjugationGroup.groupFromForms('Non-past', Adjectives__NonePast_Forms),
+        ConjugationGroup.groupFromForms('Past', Adjectives__Past_Forms),
     ]
 
     public verbConjugationGroups = [
-        new ConjugationGroup('Non-past', [
-            new ConjugationGroupItem(Verbs__NonPastShortAffirmative, new NonPastShortAffirmative()),
-            new ConjugationGroupItem(Verbs__NonPastShortNegative, new NonPastShortNegative(),),
-            new ConjugationGroupItem(Verbs__NonPastPoliteAffirmative, new NonPastPoliteAffirmative(),),
-            new ConjugationGroupItem(Verbs__NonPastPoliteNegative, new NonPastPoliteNegative()),
-        ]),
-
-        new ConjugationGroup('Past', [
-            new ConjugationGroupItem(Verbs__PastShortAffirmative, new PastShortAffirmative()),
-            new ConjugationGroupItem(Verbs__PastShortNegative, new PastShortNegative(),),
-            new ConjugationGroupItem(Verbs__PastPoliteAffirmative, new PastPoliteAffirmative(),),
-            new ConjugationGroupItem(Verbs__PastPoliteNegative, new PastPoliteNegative()),
-        ]),
-
-        new ConjugationGroup('Te-Form', [
-            new ConjugationGroupItem(Verbs__TeFormAffirmative, new TeFormAffirmative()),
-            new ConjugationGroupItem(Verbs__TeFormNegative, new TeFormNegative()),
-        ]),
-
-        new ConjugationGroup('Potential', [
-            new ConjugationGroupItem(Verbs__PotentialAffirmative, new PotentialAffirmative()),
-            new ConjugationGroupItem(Verbs__PotentialNegative, new PotentialNegative()),
-        ]),
-
-        new ConjugationGroup('Passive', [
-            new ConjugationGroupItem(Verbs__PassiveAffirmative, new PassiveAffirmative()),
-            new ConjugationGroupItem(Verbs__PassiveNegative, new PassiveNegative()),
-        ]),
-
-        new ConjugationGroup('Causative', [
-            new ConjugationGroupItem(Verbs__CausativeAffirmative, new CausativeAffirmative()),
-            new ConjugationGroupItem(Verbs__CausativeNegative, new CausativeNegative()),
-        ]),
-
-        new ConjugationGroup('Causative-Passive', [
-            new ConjugationGroupItem(Verbs__CausativePassiveAffirmative, new CausativePassiveAffirmative()),
-            new ConjugationGroupItem(Verbs__CausativePassiveNegative, new CausativePassiveNegative()),
-        ]),
-
-        new ConjugationGroup('Imperative', [
-            new ConjugationGroupItem(Verbs__ImperativeAffirmative, new ImperativeAffirmative()),
-            new ConjugationGroupItem(Verbs__ImperativeNegative, new ImperativeNegative()),
-        ]),
+        ConjugationGroup.groupFromForms('Non-past', Verbs__NonPast_Forms),
+        ConjugationGroup.groupFromForms('Past', Verbs__Past_Forms),
+        ConjugationGroup.groupFromForms('Te-Form', Verbs__TeForm_Forms),
+        ConjugationGroup.groupFromForms('Potential', Verbs__Potential_Forms),
+        ConjugationGroup.groupFromForms('Passive', Verbs__Passive_Forms),
+        ConjugationGroup.groupFromForms('Causative', Verbs__Causative_Forms),
+        ConjugationGroup.groupFromForms('Causative-Passive', Verbs__CausativePassive_Forms),
+        ConjugationGroup.groupFromForms('Imperative', Verbs__Imperative_Forms),
     ]
 
     public jlptItems = [
