ce215f493ecfbaa5fc9c1bda9bb692c5c7d5c3d3
Thorsten Kober <thorsten.kober@gmail.com>
2025-04-12 01:38:31 +0200

    carry over commit

--- src/app/srs-dialog/srs-dialog.component.ts
+++ src/app/srs-dialog/srs-dialog.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialogRef} from "@angular/material/dialog";
+import { MatDialogRef } from "@angular/material/dialog";
+import { SrsService } from '../services/srs.service';
+import { VerbForms } from '../conjugation/conjugation';
+import {
+  AdjectiveForms,
+  Adjectives__NonePast_Forms,
+  Adjectives__Past_Forms,
+  Verbs__Causative_Forms,
+  Verbs__CausativePassive_Forms,
+  Verbs__Imperative_Forms,
+  Verbs__NonPast_Forms,
+  Verbs__Passive_Forms,
+  Verbs__Past_Forms,
+  Verbs__Potential_Forms,
+  Verbs__TeForm_Forms
+} from "../conjugation/conjugation";
+import { ConjugationGroup } from '../dtos';
 
 @Component({
   selector: 'app-srs-dialog',
@@ -8,12 +24,33 @@ import {MatDialogRef} from "@angular/material/dialog";
 })
 export class SrsDialogComponent implements OnInit {
 
-  constructor(private dialogRef: MatDialogRef<SrsDialogComponent>) {
+  public adjectiveConjugationGroups = [
+          ConjugationGroup.groupFromForms('Non-past', Adjectives__NonePast_Forms),
+          ConjugationGroup.groupFromForms('Past', Adjectives__Past_Forms),
+      ]
+  
+      public verbConjugationGroups = [
+          ConjugationGroup.groupFromForms('Non-past', Verbs__NonPast_Forms),
+          ConjugationGroup.groupFromForms('Past', Verbs__Past_Forms),
+          ConjugationGroup.groupFromForms('Te-Form', Verbs__TeForm_Forms),
+          ConjugationGroup.groupFromForms('Potential', Verbs__Potential_Forms),
+          ConjugationGroup.groupFromForms('Passive', Verbs__Passive_Forms),
+          ConjugationGroup.groupFromForms('Causative', Verbs__Causative_Forms),
+          ConjugationGroup.groupFromForms('Causative-Passive', Verbs__CausativePassive_Forms),
+          ConjugationGroup.groupFromForms('Imperative', Verbs__Imperative_Forms),
+      ]
 
-   }
+  constructor(
+    private dialogRef: MatDialogRef<SrsDialogComponent>,
+    private srsService: SrsService
+  ) {
+    for (const verbFormKey in VerbForms) {
+      const verbForm = VerbForms[verbFormKey]
+
+    }
+  }
 
   ngOnInit(): void {
-
   }
 
   public close() {
